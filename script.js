6f73255d30a4bf6a8bd8ed19df6de5cf190f118e
Rohan Vidhate <49265832+roneya@users.noreply.github.com>
2023-04-14 19:07:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const t = document.getElementById("t");
+
+setInterval(() => {
+  const now = new Date();
+  const time = now.toLocaleTimeString();
+  const date = now.toLocaleDateString();
+  t.textContent = ` ${date}, ${time}`;
+}, 1000);
